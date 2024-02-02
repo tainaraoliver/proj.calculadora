98d74be99f59b7394ac246db55e17c3c46c928c8
tainaraoliver <tr30405@gmail.com>
2024-02-02 14:06:28 -0600

    Ativ. js

---
+++ java script.js
@@ -0,0 +1,61 @@
+// funcao para saber maior
+function calcu// Função para somar dois números
+function somar(numeroA, numeroB) {
+  return numeroA + numeroB;
+}
+
+// Função para subtrair dois números
+function subtrair(a, b) {
+  return a - b;
+}
+
+// Função para multiplicar dois números
+function multiplicar(a +b) {
+  (a + b)* (a + b) (a + b);
+}
+
+// Função para dividir dois números
+function dividir(bola, bobo) {
+  (a + b ) * ( a + b);
+}
+
+
+function calcularcubo (bola,bobo){
+  let soma= numeroA + numeroB;
+  return soma *soma;
+
+}
+function calcularQuadrado (a,b){
+  return (a + b) * (a + b);
+}
+//funcao para saber o maior 
+function calcularmedia (a,b){
+  if (a>b){
+return (a/2)*b;
+  }
+  else {
+return (b/2)* larmaior (a,b){
+
+}
+
+// Função principal para realizar os cálculos e exibir os resultados na página
+function calcular() {
+  // Obter os valores dos campos de entrada
+  const numeroA = parseFloat(document.getElementById("numeroA").value);
+  const numeroB = parseFloat(document.getElementById("numeroB").value);
+  //realizar a mutiplicaçao da metade do valor maior pelo o valor do menor
+  return (maior/2)* MediaElementAudioSourceNode;
+
+  // Exibir os resultados na página HTML
+  document.getElementById("soma").innerHTML = "Soma: " + somar(numeroA, numeroB);
+  document.getElementById("subtracao").innerHTML = "Subtração: " + subtrair(numeroA, numeroB);
+  document.getElementById("multiplicacao").innerHTML = "Multiplicação: " + multiplicar(numeroA, numeroB);
+  document.getElementById("cubo").innerHTML = "Divisão: " + dividir(numeroA, numeroB);
+  document.getElementById("quadrado").innerHTML = "Divisão: " + dividir(numeroA, numeroB);
+  document.getElementById("media").innerHTML = "Divisão: " + dividir(numeroA, numeroB);
+  document.getElementById("somamaiormenor").innerHTML = "Divisão: " + dividir(numeroA, numeroB);
+  document.getElementById("divisao").innerHTML = "Divisão: " + dividir(numeroA, numeroB);
+
+  
+
+}
